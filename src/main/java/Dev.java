d89e80925e033649b825baeaebe12372e3396db2
Kleiton MQ <kleiton.x5@gmail.com>
2023-04-05 20:57:24 -0300

    adicionado regras de negocio da classe Dev para poder se matricular em bootcamp e atribuir os cursos e mentorias.

--- src/main/java/Dev.java
+++ src/main/java/Dev.java
@@ -2,18 +2,33 @@ package main.java;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
+
 
 public class Dev {
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
     
-    public void inscreverBootcamp(Bootcamp bootecamp){}
+    public void inscreverBootcamp(Bootcamp bootecamp){
+        this.conteudosInscritos.addAll(bootecamp.getConteudos());
+        bootecamp.getDevInscritos().add(this);
+    }
 
-    public void progredir(){}
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+            System.err.println("Voce não está matriculado em nenhum conteúdo.");
+        }
+    }
 
-    public void calcularTotalXp(){}
+    public double calcularTotalXp(){
+        return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
+    }
 
     public void setNome(String nome){
         this.nome = nome;
