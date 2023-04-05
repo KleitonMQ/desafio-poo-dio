1e99f9567b497de80f84f212dce843fbb96f52e6
Kleiton MQ <kleiton.x5@gmail.com>
2023-04-05 12:48:29 -0300

    refatorcao das classes mentoria e curso para herdar os atributos em comum da classe abstrata conteudo.

--- src/main/java/Curso.java
+++ src/main/java/Curso.java
@@ -1,24 +1,12 @@
 package main.java;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
+    
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public int getCargaHoraria() {
@@ -31,7 +19,8 @@ public class Curso {
 
     @Override
     public String toString() {
-        return "----------------\nMentoria\n" + "titulo= " + titulo + "\n" + "descicao= " + descricao + "\n" + "cargaHoraria= " + cargaHoraria;
+        return "----------------\nMentoria\n" + "titulo= " + getTitulo() + "\n" + "descicao= " + getDescricao() + "\n" + "cargaHoraria= " + cargaHoraria;
     }
+
 
 }
